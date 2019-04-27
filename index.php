71792822d146382133dcb5ffa9b8cea14938f206
Alanna G <t_alanna@msn.com>
2019-04-27 18:21:06 -0400

    nav bar links, social media

--- index.php
+++ index.php
@@ -9,53 +9,71 @@
 	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700" rel="stylesheet">
 	<!--Custom CSS-->
 	<link rel="stylesheet" type="text/css" href="/css/main.css">
-
+	<!--font awe -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 <body>
 	<!--Start Header  -->
 	<nav class="uk-navbar-container uk-margin" uk-navbar>
-    <div class="uk-navbar-left">
+	    <div class="uk-navbar-left">
 
-        <a class="uk-navbar-item uk-logo" href="#">logo</a>
+	        <a class="uk-navbar-item uk-logo" href="#"> LOGO
+	        </a>
 
-        <ul class="uk-navbar-nav">
-            <li>
-                <a href="#">
-                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
-                    Locations
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
-                    Services
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
-                    Team
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
-                    Contact
-                </a>
-            </li>
-        </ul>
+	        <ul class="uk-navbar-nav">
+	            <li>
+	                <a href="#">
+	                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
+	                    Locations
+	                </a>
+	            </li>
+	            <li>
+	                <a href="#">
+	                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
+	                    Services
+	                </a>
+	            </li>
+	            <li>
+	                <a href="#">
+	                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
+	                    Team
+	                </a>
+	            </li>
+	            <li>
+	                <a href="#">
+	                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
+	                    Contact
+	                </a>
+	            </li>
+	        </ul>
 
-        <div class="uk-navbar-item">
-            <div>Some <a href="#">Link</a></div>
-        </div>
-    </div>
+	        <div class="uk-navbar-item">
+	            <div>Some <a href="#">Link</a></div>
+	        </div>
+	    </div>
+
+	    <div class="uk-navbar-right">
+
+			        <ul class="uk-navbar-nav">
+			            <li class="uk-active"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+			            <li>
+			                <a href="#"><i class="fab fa-twitter"></i></a>
+			            </li>
+			            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+			        </ul>
+
+			    </div>
 </nav>
 
 	<!--End Header  -->
 
+<div class="uk-container uk-container-large">
+	<hr class="uk-divider-icon">
+
 	<button class="uk-button uk-button-primary">DEMO</button>
 	<br>	
 		Hello Jody
+</div>
 
 <!--JQuery link-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
