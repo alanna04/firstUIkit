a19ae221bb5d6ab75aba4f731fa50659ed23c1ff
Alanna G <t_alanna@msn.com>
2019-04-27 16:57:06 -0400

    header, header css

--- index.php
+++ index.php
@@ -6,15 +6,56 @@
 	<!--UI kit CSS-->
 	<link rel="stylesheet" type="text/css" href="/css/uikit.min.css">
 	<!--Google Fonts-->
-	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700" rel="stylesheet">
 	<!--Custom CSS-->
 	<link rel="stylesheet" type="text/css" href="/css/main.css">
 
 </head>
 <body>
-Hello
-<br>
-Jody
+	<!--Start Header  -->
+	<nav class="uk-navbar-container uk-margin" uk-navbar>
+    <div class="uk-navbar-left">
+
+        <a class="uk-navbar-item uk-logo" href="#">logo</a>
+
+        <ul class="uk-navbar-nav">
+            <li>
+                <a href="#">
+                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
+                    Locations
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
+                    Services
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
+                    Team
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
+                    Contact
+                </a>
+            </li>
+        </ul>
+
+        <div class="uk-navbar-item">
+            <div>Some <a href="#">Link</a></div>
+        </div>
+    </div>
+</nav>
+
+	<!--End Header  -->
+
+	<button class="uk-button uk-button-primary">DEMO</button>
+	<br>	
+		Hello Jody
 
 <!--JQuery link-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
