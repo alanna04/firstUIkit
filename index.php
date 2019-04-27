5f839b23811420e48b2b77bf07fe05a99b4aa4dc
Alanna G <t_alanna@msn.com>
2019-04-27 16:06:57 -0400

    links google fonts, body style, php doc

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UIkit Site</title>
+
+	<!--UI kit CSS-->
+	<link rel="stylesheet" type="text/css" href="/css/uikit.min.css">
+	<!--Google Fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+	<!--Custom CSS-->
+	<link rel="stylesheet" type="text/css" href="/css/main.css">
+
+</head>
+<body>
+Hello
+<br>
+Jody
+
+<!--JQuery link-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<!--JS UI kit-->
+<script type="text/javascript" src="js/uikit.min.js"></script>
+</body>
+</html>
